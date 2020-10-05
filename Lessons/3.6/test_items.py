bdaf3ca83af7a8c82a77eb4e86561f2d14ed5e24
memberember <64272753+memberember@users.noreply.github.com>
2020-10-05 15:21:01 +0500

    Переименовал функцию теста в более понятную - Дмитрий Тен 05.10.2020

--- Lessons/3.6/test_items.py
+++ Lessons/3.6/test_items.py
@@ -2,7 +2,7 @@ import time
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 
-def test_guest_should_see_login_link(browser):
+def test_guest_should_see_add_to_basket_button(browser):
     browser.get(link)
     time.sleep(30)
     assert browser.find_element_by_class_name('btn-add-to-basket')
