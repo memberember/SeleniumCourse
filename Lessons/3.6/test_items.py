7ae420aba2cf5f0b5b50b8b9faea7eff0b48752f
memberember <64272753+memberember@users.noreply.github.com>
2020-10-05 15:17:04 +0500

    Залил последний таск для раздела 3.6 - Дмитрий Тен 05.10.2020

---
+++ Lessons/3.6/test_items.py
@@ -0,0 +1,8 @@
+import time
+link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+
+
+def test_guest_should_see_login_link(browser):
+    browser.get(link)
+    time.sleep(10)
+    assert browser.find_element_by_class_name('btn-add-to-basket')
