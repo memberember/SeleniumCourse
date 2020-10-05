cb8019252208e4a9c4f59158df0eee87b9a887c3
memberember <64272753+memberember@users.noreply.github.com>
2020-10-05 15:19:38 +0500

    Оставил возможность запуска через хром - Дмитрий Тен 05.10.2020

--- Lessons/3.6/test_items.py
+++ Lessons/3.6/test_items.py
@@ -4,5 +4,5 @@ link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 def test_guest_should_see_login_link(browser):
     browser.get(link)
-    time.sleep(10)
+    time.sleep(30)
     assert browser.find_element_by_class_name('btn-add-to-basket')
